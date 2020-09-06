148a8dfe61e5b08f1970c1c57e6db4e2f93c7882
Raheem Shah <Raheemshah2809@gmail.com>
2020-09-06 19:27:59 +0100

    added js file

---
+++ scripts/main.js
@@ -0,0 +1,12 @@
+ 
+  @keyframes rotate-three {
+    0% {
+      transform: rotateX(35deg) rotateY(55deg) rotateZ(0deg);
+    }
+    100% {
+      transform: rotateX(35deg) rotateY(55deg) rotateZ(360deg);
+    }
+  }
+  $(window).on("load",function(){
+    $(".loader-wrapper").fadeOut("slow");
+});
