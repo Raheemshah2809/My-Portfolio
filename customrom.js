4b269efc8476a732414270144812365250a2d542
Raheemshah2809 <66431658+Raheemshah2809@users.noreply.github.com>
2020-10-19 00:43:30 +0100

    new alert box

--- customrom.js
+++ customrom.js
@@ -1,59 +1,23 @@
-(function(){
-  var $content = $('.modal_info').detach();
+// Click function for show the Modal
 
-  $('.open_button').on('click', function(e){
-    modal.open({
-      content: $content,
-      width: 540,
-      height: 270,
-    });
-    $content.addClass('modal_content');
-    $('.modal, .modal_overlay').addClass('display');
-    $('.open_button').addClass('load');
-  });
-}());
+$(".show").on("click", function(){
+  $(".mask").addClass("active");
+});
 
-var modal = (function(){
+// Function for close the Modal
 
-  var $close = $('<button role="button" class="modal_close" title="Close"><span></span></button>');
-  var $content = $('<div class="modal_content"/>');
-  var $modal = $('<div class="modal"/>');
-  var $window = $(window);
+function closeModal(){
+  $(".mask").removeClass("active");
+}
 
-  $modal.append($content, $close);
+// Call the closeModal function on the clicks/keyboard
 
-  $close.on('click', function(e){
-    $('.modal, .modal_overlay').addClass('conceal');
-    $('.modal, .modal_overlay').removeClass('display');
-    $('.open_button').removeClass('load');
-    e.preventDefault();
-    modal.close();
-  });
+$(".close, .mask").on("click", function(){
+  closeModal();
+});
 
-  return {
-    center: function(){
-      var top = Math.max($window.height() - $modal.outerHeight(), 0) / 2;
-      var left = Math.max($window.width() - $modal.outerWidth(), 0) / 2;
-      $modal.css({
-        top: top + $window.scrollTop(),
-        left: left + $window.scrollLeft(),
-      });
-    },
-    open: function(settings){
-      $content.empty().append(settings.content);
-
-      $modal.css({
-        width: settings.width || 'auto',
-        height: settings.height || 'auto'
-      }).appendTo('body');
-
-      modal.center();
-      $(window).on('resize', modal.center);
-    },
-    close: function(){
-      $content.empty();
-      $modal.detach();
-      $(window).off('resize', modal.center);
-    }
-  };
-}());
+$(document).keyup(function(e) {
+  if (e.keyCode == 27) {
+    closeModal();
+  }
+});
