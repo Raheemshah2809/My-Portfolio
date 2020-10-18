b02d31a947ca5e6088039f28974d81391bdbb10b
Raheemshah2809 <66431658+Raheemshah2809@users.noreply.github.com>
2020-10-19 00:48:54 +0100

    back to first popup

--- customrom.js
+++ customrom.js
@@ -1,23 +1,33 @@
-// Click function for show the Modal
-
-$(".show").on("click", function(){
-  $(".mask").addClass("active");
-});
-
-// Function for close the Modal
-
-function closeModal(){
-  $(".mask").removeClass("active");
-}
-
-// Call the closeModal function on the clicks/keyboard
-
-$(".close, .mask").on("click", function(){
-  closeModal();
-});
-
-$(document).keyup(function(e) {
-  if (e.keyCode == 27) {
-    closeModal();
+function CustomAlert(){
+    this.alert = function(message,title){
+      document.body.innerHTML = document.body.innerHTML + '<div id="dialogoverlay"></div><div id="dialogbox" class="slit-in-vertical"><div><div id="dialogboxhead"></div><div id="dialogboxbody"></div><div id="dialogboxfoot"></div></div></div>';
+  
+      let dialogoverlay = document.getElementById('dialogoverlay');
+      let dialogbox = document.getElementById('dialogbox');
+      
+      let winH = window.innerHeight;
+      dialogoverlay.style.height = winH+"px";
+      
+      dialogbox.style.top = "100px";
+  
+      dialogoverlay.style.display = "block";
+      dialogbox.style.display = "block";
+      
+      document.getElementById('dialogboxhead').style.display = 'block';
+  
+      if(typeof title === 'undefined') {
+        document.getElementById('dialogboxhead').style.display = 'none';
+      } else {
+        document.getElementById('dialogboxhead').innerHTML = '<i class="fa fa-exclamation-circle" aria-hidden="true"></i> '+ title;
+      }
+      document.getElementById('dialogboxbody').innerHTML = message;
+      document.getElementById('dialogboxfoot').innerHTML = '<button class="pure-material-button-contained active" onclick="customAlert.ok()">OK</button>';
+    }
+    
+    this.ok = function(){
+      document.getElementById('dialogbox').style.display = "none";
+      document.getElementById('dialogoverlay').style.display = "none";
+    }
   }
-});
+  
+  let customAlert = new CustomAlert();
