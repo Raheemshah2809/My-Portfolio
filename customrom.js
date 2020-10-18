00be3afe198511630950a952c8461039f0879747
Raheemshah2809 <66431658+Raheemshah2809@users.noreply.github.com>
2020-10-19 00:35:46 +0100

    chnaged alert box

--- customrom.js
+++ customrom.js
@@ -1,33 +1,59 @@
-function CustomAlert(){
-    this.alert = function(message,title){
-      document.body.innerHTML = document.body.innerHTML + '<div id="dialogoverlay"></div><div id="dialogbox" class="slit-in-vertical"><div><div id="dialogboxhead"></div><div id="dialogboxbody"></div><div id="dialogboxfoot"></div></div></div>';
-  
-      let dialogoverlay = document.getElementById('dialogoverlay');
-      let dialogbox = document.getElementById('dialogbox');
-      
-      let winH = window.innerHeight;
-      dialogoverlay.style.height = winH+"px";
-      
-      dialogbox.style.top = "100px";
-  
-      dialogoverlay.style.display = "block";
-      dialogbox.style.display = "block";
-      
-      document.getElementById('dialogboxhead').style.display = 'block';
-  
-      if(typeof title === 'undefined') {
-        document.getElementById('dialogboxhead').style.display = 'none';
-      } else {
-        document.getElementById('dialogboxhead').innerHTML = '<i class="fa fa-exclamation-circle" aria-hidden="true"></i> '+ title;
-      }
-      document.getElementById('dialogboxbody').innerHTML = message;
-      document.getElementById('dialogboxfoot').innerHTML = '<button class="pure-material-button-contained active" onclick="customAlert.ok()">OK</button>';
+(function(){
+  var $content = $('.modal_info').detach();
+
+  $('.open_button').on('click', function(e){
+    modal.open({
+      content: $content,
+      width: 540,
+      height: 270,
+    });
+    $content.addClass('modal_content');
+    $('.modal, .modal_overlay').addClass('display');
+    $('.open_button').addClass('load');
+  });
+}());
+
+var modal = (function(){
+
+  var $close = $('<button role="button" class="modal_close" title="Close"><span></span></button>');
+  var $content = $('<div class="modal_content"/>');
+  var $modal = $('<div class="modal"/>');
+  var $window = $(window);
+
+  $modal.append($content, $close);
+
+  $close.on('click', function(e){
+    $('.modal, .modal_overlay').addClass('conceal');
+    $('.modal, .modal_overlay').removeClass('display');
+    $('.open_button').removeClass('load');
+    e.preventDefault();
+    modal.close();
+  });
+
+  return {
+    center: function(){
+      var top = Math.max($window.height() - $modal.outerHeight(), 0) / 2;
+      var left = Math.max($window.width() - $modal.outerWidth(), 0) / 2;
+      $modal.css({
+        top: top + $window.scrollTop(),
+        left: left + $window.scrollLeft(),
+      });
+    },
+    open: function(settings){
+      $content.empty().append(settings.content);
+
+      $modal.css({
+        width: settings.width || 'auto',
+        height: settings.height || 'auto'
+      }).appendTo('body');
+
+      modal.center();
+      $(window).on('resize', modal.center);
+    },
+    close: function(){
+      $content.empty();
+      $modal.detach();
+      $(window).off('resize', modal.center);
     }
-    
-    this.ok = function(){
-      document.getElementById('dialogbox').style.display = "none";
-      document.getElementById('dialogoverlay').style.display = "none";
-    }
-  }
-  
-  let customAlert = new CustomAlert();
+  };
+}());
