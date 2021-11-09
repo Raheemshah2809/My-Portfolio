39883042b51ce13252a4cefa01cf0bfea3e26ca2
Raheem Shah <Raheemshah2809@gmail.com>
2021-11-09 11:51:00 +0000

    2.9.1

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,27 +1,27 @@
-/*===== MENU SHOW =====*/ 
-const showMenu = (toggleId, navId) =>{
+/*===== MENU SHOW =====*/
+const showMenu = (toggleId, navId) => {
     const toggle = document.getElementById(toggleId),
-    nav = document.getElementById(navId)
+        nav = document.getElementById(navId)
 
-    if(toggle && nav){
-        toggle.addEventListener('click', ()=>{
+    if (toggle && nav) {
+        toggle.addEventListener('click', () => {
             nav.classList.toggle('show')
         })
     }
 }
-showMenu('nav-toggle','nav-menu')
+showMenu('nav-toggle', 'nav-menu')
 
 /*===== ACTIVE AND REMOVE MENU =====*/
-const navLink = document.querySelectorAll('.nav__link');   
+const navLink = document.querySelectorAll('.nav__link');
 
-function linkAction(){
-  /*Active link*/
-  navLink.forEach(n => n.classList.remove('active'));
-  this.classList.add('active');
-  
-  /*Remove menu mobile*/
-  const navMenu = document.getElementById('nav-menu')
-  navMenu.classList.remove('show')
+function linkAction() {
+    /*Active link*/
+    navLink.forEach(n => n.classList.remove('active'));
+    this.classList.add('active');
+
+    /*Remove menu mobile*/
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction));
 
@@ -34,44 +34,59 @@ const sr = ScrollReveal({
 });
 
 /*SCROLL HOME*/
-sr.reveal('.home__title',{}); 
-sr.reveal('.button',{delay: 200}); 
-sr.reveal('.home__img',{delay: 400}); 
-sr.reveal('.home__social-icon',{ interval: 200}); 
+sr.reveal('.home__title', {});
+sr.reveal('.button', {
+    delay: 200
+});
+sr.reveal('.home__img', {
+    delay: 400
+});
+sr.reveal('.home__social-icon', {
+    interval: 200
+});
 
 /*SCROLL ABOUT*/
-sr.reveal('.about__img',{}); 
-sr.reveal('.about__subtitle',{delay: 400}); 
-sr.reveal('.about__text',{delay: 400}); 
+sr.reveal('.about__img', {});
+sr.reveal('.about__subtitle', {
+    delay: 400
+});
+sr.reveal('.about__text', {
+    delay: 400
+});
 
 /*SCROLL SKILLS*/
-sr.reveal('.skills__subtitle',{}); 
-sr.reveal('.skills__text',{}); 
-sr.reveal('.skills__data',{interval: 200}); 
-sr.reveal('.skills__img',{delay: 600});
+sr.reveal('.skills__subtitle', {});
+sr.reveal('.skills__text', {});
+sr.reveal('.skills__data', {
+    interval: 200
+});
+sr.reveal('.skills__img', {
+    delay: 600
+});
 
 /*SCROLL WORK*/
-sr.reveal('.work__img',{interval: 200}); 
+sr.reveal('.work__img', {
+    interval: 200
+});
 
 /*SCROLL CONTACT*/
-sr.reveal('.contact__input',{interval: 200}); 
+sr.reveal('.contact__input', {
+    interval: 200
+});
 
 const chk = document.getElementById('chk');
 
 chk.addEventListener('change', () => {
-	document.body.classList.toggle('dark');
+    document.body.classList.toggle('dark');
 });
 
 
 
 floating_btn.addEventListener('click', () => {
-	social_panel_container.classList.toggle('visible')
+    social_panel_container.classList.toggle('visible')
 });
 
 close_btn.addEventListener('click', () => {
-	social_panel_container.classList.remove('visible')
+    social_panel_container.classList.remove('visible')
 });
-
-
-
 
